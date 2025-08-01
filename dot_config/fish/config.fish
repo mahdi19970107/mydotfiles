905ee132cd76def91db8730f7513c7337d1bff17
mahdi19970107 <mahdi.chamani20@gmail.com>
2025-08-01 17:42:35 +0330

    .

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,30 +4,20 @@ if status is-interactive
     end
 end
 
-function should_load_tools
-    # Load if not tty, or if SSH, or if running in Termux
-    if not string match -q '/dev/tty*' (tty)
-        return 0
+if type -q fastfetch
+    if not set -q SSH_TTY; and not string match -q '/dev/tty*' (tty)
+        fastfetch
     end
-    if set -q SSH_TTY
-        return 0
-    end
-    if set -q TERMUX_VERSION
-        return 0
-    end
-    return 1
 end
 
-if type -q fastfetch
-    should_load_tools; and fastfetch
-end
+    set -U fish_greeting
 
 if type -q zoxide
-    should_load_tools; and zoxide init fish | source
+    zoxide init fish | source
 end
 
 if type -q thefuck
-    should_load_tools; and thefuck --alias | source
+    thefuck --alias | source
 end
 
 # Load custom aliases
